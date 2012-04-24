1508679e44b3b43e5abb3ddfb85347c35432bb47
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-24 14:30:43 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-image-slideshow/trunk@535491 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ help.php
@@ -0,0 +1,15 @@
+<div class="wrap">
+  <h2>Plugin Management </h2>
+  Click "Go to - Images Management" button to add/update/delete the images. <br />
+  Click "Go to - Gallery Setting" button to update gallery setting.
+  <h2>Plugin Configuration</h2>
+  <ul>
+  <li>1. Drag and Drop the Widget</li>
+  <li>2. Add directly in the theme</li>
+  <li>3. short code for pages and posts</li>
+  </ul>
+  <a target="_blank" href='http://www.gopiplus.com/work/2011/05/06/wordpress-plugin-wp-image-slideshow/'>Click here for more help</a>
+  <h2>About Plugin</h2>
+  Plug-in created by <a target="_blank" href='http://www.gopiplus.com/work/'>www.gopiplus.com</a>.<br />
+  <a target="_blank" href='http://www.gopiplus.com/work/2011/05/06/wordpress-plugin-wp-image-slideshow/'>Click here</a> to see more information and live demo.<br />
+</div>
